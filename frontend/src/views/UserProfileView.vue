75ee87d5b734f046d3da5ea4578b6298b1ae3211
Lubos Kocman <Lubos.Kocman@suse.com>
2026-03-13 00:21:22 +0100

    Use 200px previews on earned badges

--- frontend/src/views/UserProfileView.vue
+++ frontend/src/views/UserProfileView.vue
@@ -71,7 +71,7 @@ SPDX-License-Identifier: Apache-2.0
             class="badge-card"
             :aria-label="`View details for ${b.title} badge`"
           >
-            <img :src="b.picture" :alt="b.title" class="badge-image" />
+            <img :src="b.picture.replace('/badges/', '/badges/200px/')" :alt="b.title" class="badge-image" />
           </router-link>
           <div class="badge-title">
             {{ b.title }}
